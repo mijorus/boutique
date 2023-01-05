be223c2ea574e9d1f3a604b9fe57a95d65e1fb4b
Lorenzo Paderi <lorenzo.mijorus@gmail.com>
2023-01-06 00:53:10 +0100

    wip

--- src/UpdatesList.py
+++ src/UpdatesList.py
@@ -14,6 +14,7 @@ from .components.CustomComponents import NoAppsFoundRow
 from .components.AppListBoxItem import AppListBoxItem
 from .lib.utils import set_window_cursor, key_in_dict, log
 
+
 class UpdatesList(Gtk.ScrolledWindow):
     def __init__(self):
         super().__init__()
@@ -30,22 +31,27 @@ class UpdatesList(Gtk.ScrolledWindow):
         self.updates_fetched = False
         self.updates_row = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, visible=True)
 
-        ## the list box containing all the updatable apps
+        # the list box containing all the updatable apps
         self.updates_row_list_items = []
         self.updates_row_list = Gtk.ListBox(css_classes=["boxed-list"], margin_bottom=25)
-        self.updates_row_list_spinner = Gtk.ListBoxRow(child=Gtk.Spinner(spinning=True, margin_top=5, margin_bottom=5), visible=False)
-        self.updates_row_list.append(self.updates_row_list_spinner)
 
-        updates_title_row = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, valign=Gtk.Align.CENTER, margin_bottom=5)
+        
+        self.updates_title_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        
+        self.updates_title_row = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, valign=Gtk.Align.CENTER, margin_bottom=5)
+        self.updates_title_container.append(self.updates_title_row)
+        
+        self.updates_row_list_spinner = Gtk.Spinner(spinning=True, margin_start=5, visible=False)
+        self.updates_title_container.append(self.updates_row_list_spinner)
 
         self.updates_title_label = Gtk.Label(label='', css_classes=['title-4'], hexpand=True, halign=Gtk.Align.START)
-        updates_title_row.append( self.updates_title_label )
-        
-        self.update_all_btn = Gtk.Button(label='Update all', css_classes=['suggested-action'], valign=Gtk.Align.CENTER, visible=False) 
+        self.updates_title_row.append(self.updates_title_label)
+
+        self.update_all_btn = Gtk.Button(label='Update all', css_classes=['suggested-action'], valign=Gtk.Align.CENTER, visible=False)
         self.update_all_btn.connect('clicked', self.on_update_all_btn_clicked)
 
-        updates_title_row.append(self.update_all_btn)
-        self.updates_row.append(updates_title_row)
+        self.updates_title_row.append(self.update_all_btn)
+        self.updates_row.append(self.updates_title_container)
         self.updates_row.append(self.updates_row_list)
 
         self.main_box.append(messages_row)
@@ -54,26 +60,26 @@ class UpdatesList(Gtk.ScrolledWindow):
         clamp = Adw.Clamp(child=self.main_box, maximum_size=600, margin_top=20, margin_bottom=20)
         self.set_child(clamp)
 
-    def on_show(self, only_provider: Optional[str]=None):
+    def on_show(self, only_provider: Optional[str] = None):
         if self.busy:
             return
-    
+
         self.busy = True
-    
+
         for widget in self.updates_row_list_items:
             self.updates_row_list.remove(widget)
 
-        self.updates_title_label.set_label('Searching for updates...')
+        # self.updates_title_label.set_label('Searching for updates...')
+        self.toggle_updates_title_label_state('Searching for updates...', True)
         self.updates_row_list_spinner.set_visible(True)
         self.updates_row_list.set_css_classes(["boxed-list"])
-    
+
         threading.Thread(target=self.async_load_upgradable, daemon=True).start()
 
     # Runs the background task to check for app updates
-    def async_load_upgradable(self, only_provider: Optional[str]=None):
+    def async_load_upgradable(self, only_provider: Optional[str] = None):
         updatable_elements = []
 
-        
         for p, provider in providers.items():
             apps = provider.list_installed()
 
@@ -81,8 +87,8 @@ class UpdatesList(Gtk.ScrolledWindow):
                 for a in apps:
                     if (a.id == upg.id):
                         upg.extra_data['app_list_element'] = a
-                        
-                        if a.extra_data and ('version' in a.extra_data) and  a.extra_data['version'] != upg.to_version:
+
+                        if a.extra_data and ('version' in a.extra_data) and a.extra_data['version'] != upg.to_version:
                             upg.to_version = a.extra_data['version'] + ' > ' + upg.to_version
 
                         updatable_elements.append(upg)
@@ -93,7 +99,7 @@ class UpdatesList(Gtk.ScrolledWindow):
 
     def render_updatables(self, updatable_elements: List[AppUpdateElement]):
         upgradable_count = 0
-    
+
         for upg in updatable_elements:
             update_is_an_app = False
 
@@ -103,21 +109,50 @@ class UpdatesList(Gtk.ScrolledWindow):
             app_list_item = AppListBoxItem(list_element, activatable=False, selectable=False, hexpand=True)
             GLib.idle_add(app_list_item.load_icon)
 
-            self.updates_row_list.append( app_list_item )
-            self.updates_row_list_items.append( app_list_item )
+            self.updates_row_list.append(app_list_item)
+            self.updates_row_list_items.append(app_list_item)
 
         self.updates_fetched = True
         self.updates_row_list_spinner.set_visible(False)
-        
+
         if updatable_elements:
-            self.update_all_btn.set_visible(True)
-            self.updates_title_label.set_label('Available updates')
+            self.toggle_updates_title_label_state('Available updates', False)
         else:
-            self.updates_title_label.set_label('Everything is up to date!')
-            self.update_all_btn.set_visible(False)
-            self.updates_row_list.set_css_classes([])
+            self.toggle_updates_title_label_state('Everything is up to date!', True)
+            # self.update_all_btn.set_visible(False)
+
+            # self.updates_title_row.set_vexpand(True)
+            # self.updates_title_row.set_halign(Gtk.Align.CENTER)
+
+            # self.updates_title_label.set_label('Everything is up to date!')
+            # self.updates_title_label.set_css_classes(['title-3'])
+            # self.updates_row_list.set_css_classes([])
 
         self.busy = False
+
+    def toggle_updates_title_label_state(self, label: str, show_as_title: bool):
+        self.updates_title_label.set_label(label)
+
+        if show_as_title:
+            self.update_all_btn.set_visible(False)
+            self.updates_title_label.set_css_classes(['title-3'])
+
+            self.updates_title_container.set_vexpand(True)
+            self.updates_title_container.set_halign(Gtk.Align.CENTER)
+            self.updates_title_container.set_valign(Gtk.Align.CENTER)
+
+            self.updates_row_list.set_visible(False)
+        else:
+            self.updates_row_list.set_visible(True)
+            self.updates_row_list.set_css_classes([])
+
+            self.updates_title_label.set_css_classes([])
+
+            self.updates_title_container.set_vexpand(False)
+            self.updates_title_container.set_halign(Gtk.Align.START)
+            self.updates_title_container.set_valign(Gtk.Align.START)
+
+            self.update_all_btn.set_visible(True)
 
     def after_update_all(self, result: bool, prov: str):
         if result and (not self.update_all_btn.has_css_class('destructive-action')):
